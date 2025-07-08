116a3814bf489f81dfee0aaf24de80677caeee65
Andres Martinez <andres.ok30@hotmail.com>
2025-07-08 13:02:45 -0500

    Sistema de Inventarios

---
+++ src/main/java/com/nexos/sotfware/Inventario/Automotriz/InventarioAutomotrizApplication.java
@@ -0,0 +1,13 @@
+package com.nexos.sotfware.Inventario.Automotriz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioAutomotrizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioAutomotrizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexos/sotfware/Inventario/Automotriz/InventarioAutomotrizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexos.sotfware.Inventario.Automotriz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioAutomotrizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
